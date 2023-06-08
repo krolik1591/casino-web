22f07a244e0f14f907b62996180af2b580c49da6
svin <svinerus@gmail.com>
2023-06-08 23:37:14 +0300

    Extract components in WOF.tsx

--- src/pages/WOF/WOF.tsx
+++ src/pages/WOF/WOF.tsx
@@ -1,11 +1,40 @@
 import React, {useEffect} from 'react';
-import {Button, Form, FormGroup} from "react-bootstrap";
+import {Button, Form, InputGroup} from "react-bootstrap";
 import {backend} from "../../helpers/backend";
 import {useAuthUser} from "react-auth-kit";
 
 function WOF() {
     const auth = useAuthUser()
+    const [wheel, setWheel] = React.useState(undefined)
 
+    useEffect(() => {
+        backend("/get_fortune_wheel", auth()).then(setWheel)
+    }, [])
+
+    async function handleSubmit(input) {
+        alert(JSON.stringify(input))
+        const res = await backend("/create_fortune_wheel", auth(), input)
+        console.log(res)
+    }
+
+    if (wheel)
+        return <ShowWheel wheel={wheel}/>
+    return <CreateForm onSubmit={handleSubmit}/>
+}
+
+
+function ShowWheel({wheel}) {
+    return (
+      <div className="container">
+          <h1> Current wheel of fortune: </h1>
+          <p> Ticket cost: {wheel.ticket_cost} </p>
+          <p> Creation date: {wheel.date_creature} </p>
+          <p> End date: {wheel.date_end} </p>
+      </div>
+    )
+}
+
+function CreateForm({onSubmit}) {
     const [input, setInput] = React.useState({
         ticket_cost: 100,
         end_date: 0,
@@ -13,59 +42,49 @@ function WOF() {
         winners: 1,
         distribution: "70, 20, 10"
     })
-    const [wheelExist, setWheelExist] = React.useState(undefined)
-
-    useEffect(() => {
-        backend("/get_fortune_wheel", auth()).then(setWheelExist)
-    }, [])
 
     async function handleSubmit(event) {
         event.preventDefault()
-        alert(JSON.stringify(input))
-
-        const res = await backend("/create_fortune_wheel", auth(), input)
-        console.log(res)
+        onSubmit(input)
     }
 
     const handleChange = ({target}) => setInput(values => ({...values, [target.name]: target.value}))
 
-    if (wheelExist) {
-        return (
-          <div className="container">
-              <h1> Wheel of fortune already exist </h1>
-              <h2> Ticket cost: {wheelExist.ticket_cost} </h2>
-              <h2> End date: {wheelExist.date_creature} </h2>
-              <h2> End date: {wheelExist.date_end} </h2>
-          </div>
-        )
-    }
+
     return (
       <div className="container">
 
           <h1> Create wheel of fortune </h1>
           <Form onSubmit={handleSubmit}>
-              <FormGroup>
-                  <Form.Label> Ticket cost </Form.Label>
+              <MyInput label={" Ticket cost "}>
                   <Form.Control type="number" name={'ticket_cost'} value={input.ticket_cost} onChange={handleChange}/>
-              </FormGroup>
-              <FormGroup>
-                  <Form.Label> When spin? </Form.Label>
+              </MyInput>
+              <MyInput label={" When spin? "}>
                   <Form.Control type="datetime-local" name={'end_date'} value={input.end_date} onChange={handleChange}/>
-              </FormGroup>
-              <FormGroup>
-                  <Form.Label>Commission (%)</Form.Label>
+              </MyInput>
+              <MyInput label={"Commission"}>
                   <Form.Control type="number" name="commission" value={input.commission} onChange={handleChange}/>
-              </FormGroup>
-              <FormGroup>
-                  <Form.Label>Winning Distribution</Form.Label>
+                  <InputGroup.Text> % </InputGroup.Text>
+              </MyInput>
+              <MyInput label={"Winning Distribution"}>
                   <Form.Control type="text" name={'distribution'} value={input.distribution} onChange={handleChange}/>
-              </FormGroup>
-              <br/>
+              </MyInput>
+
               <Button type='submit'> Create </Button>
           </Form>
 
       </div>
     );
+}
+
+
+function MyInput({children, label}) {
+    return <>
+        <Form.Label>{label}</Form.Label>
+        <InputGroup className="mb-3">
+            {children}
+        </InputGroup>
+    </>
 }
 
 export default WOF;
