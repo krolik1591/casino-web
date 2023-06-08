66d0dad4729b31f939e27271574e57ea0c48b345
svin <svinerus@gmail.com>
2023-06-09 00:19:44 +0300

    Use https backend

--- src/helpers/backend.ts
+++ src/helpers/backend.ts
@@ -4,6 +4,7 @@ export async function backend(url: string, auth: any, data?: any) {
     const authString = encodeURIComponent(JSON.stringify(auth))
 
     const init: RequestInit = {
+        mode: "cors",
         headers: {
             "Content-Type": "application/json",
             "X-Auth": authString,
