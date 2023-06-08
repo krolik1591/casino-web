e844899b275ec1455763e26e42445179ab5f89b0
svin <svinerus@gmail.com>
2023-06-08 20:56:51 +0300

    Add login via telegram

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import TelegramLoginButton from 'react-telegram-login';
 
 export default function Index() {
-    return <div>Hi</div>
+    return <div>
+        Hi
+        <Login botName={"stillnowoman_bot"}/>
+    </div>
+}
+
+
+function Login({botName}) {
+    const handleTelegramResponse = (response) => {
+        console.log(response)
+    }
+    return <TelegramLoginButton dataOnauth={handleTelegramResponse} botName={botName} />
 }
