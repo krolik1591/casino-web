6a008ded74cd4a3897cd7c08e6954f15d0755ac7
svin <svinerus@gmail.com>
2023-06-08 23:37:23 +0300

    Add nav links

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -20,6 +20,11 @@ function MyNavbar() {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
         <Nav.Link as={NavLink} to="wof">Wheel of fortune</Nav.Link>
+        <Nav.Link as={NavLink} to="promo">Promo-codes</Nav.Link>
+        <Nav.Link as={NavLink} to="broadcast">Broadcast</Nav.Link> {/* Розсилки  */}
+        <Nav.Link as={NavLink} to="users">Users</Nav.Link> {/* Змінити баланс юзера (демо, реальний, промо, реферальний, лотерейний), Заблокувати юзера,  Задати кастомний реферальний рівень юзера  */}
+        <Nav.Link as={NavLink} to="settings">Settings</Nav.Link>
+        <Nav.Link as={NavLink} to="stats">Stats</Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
