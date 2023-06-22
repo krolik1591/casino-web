78e4c5bcc64076840ff8f03595db06b569657c1c
krolik1591 <dasenkosasa19@gmail.com>
2023-06-22 16:31:43 +0300

    add possibility to buy winner tickets for admin while WoF exist

--- src/pages/WOF/WOF.tsx
+++ src/pages/WOF/WOF.tsx
@@ -9,7 +9,7 @@ function WOF() {
     const [wheel, setWheel] = React.useState(undefined)
 
     useEffect(() => {
-        console.log(auth().id)
+        // console.log(auth().id)
 
         backend("/get_fortune_wheel", auth()).then(setWheel)
     }, [])
@@ -23,17 +23,31 @@ function WOF() {
 
 function ShowWheel({info}) {
     const wheel = info.wheel;
+    const winners = info.winners_info
+    const auth = useAuthUser()
+
+    const handleBuyTicket = async () => {
+        const addTicket = {'admin_id': auth().id}
+        const res = await backend("/add_win_ticket", auth(), addTicket)
+        console.log(res)
+    };
+
+
+    console.log(winners)
     return (
-      <div className="container">
-          <h1> Current wheel of fortune: </h1>
-          <p> Ticket cost: {wheel.ticket_cost} </p>
-          <p> Commission: {wheel.commission} </p>
-          <p> Rewards: {wheel.rewards} </p>
-          <p> Winners: {wheel.winners} </p>
-          <p> Creation date: {wheel.timestamp_start} </p>
-          <p> End date: {wheel.timestamp_end} </p>
-          <ShowTickets tickets={info.tickets}/>
-      </div>
+        <div className="container">
+            <h1> Current wheel of fortune: </h1>
+            <p> Ticket cost: {wheel.ticket_cost} </p>
+            <p> Commission: {wheel.commission} </p>
+            <p> Rewards: {wheel.rewards} </p>
+            <p> Creation date: {wheel.timestamp_start} </p>
+            <p> End date: {wheel.timestamp_end} </p>
+            {winners.map((winner, index) => (
+                <p key={index}> Nearest winner {index+1}: {winner[1]}, Ticket number: {winner[0]} </p>
+            ))}
+            <button onClick={handleBuyTicket}>Купити виграшний білет</button>
+            <ShowTickets tickets={info.tickets}/>
+        </div>
     )
 }
 
