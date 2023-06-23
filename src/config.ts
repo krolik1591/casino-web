6c2c9cd3658e837f872e321fd707aa07d4511e60
svin <svinerus@gmail.com>
2023-06-23 21:07:37 +0300

    Use prod domain if not devMode

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,6 @@
-export const backendUrl = "https://localhost:8080";
-export const botUsername = "myCasinoTestbot"
+export const devMode = process.env.NODE_ENV === "development"
+if (devMode) console.log("Running in development mode");
+
+export const backendUrl = devMode ? "http://localhost:8080" : "https://tongames.online";
+export const botUsername =  devMode ? "stillnowoman_bot" : "myCasinoTestbot";
+
